0e11f2cec0f82ef2e53138475988e1a702bd7053
jmkitavi <kitavimjoseph@gmail.com>
2020-04-28 19:48:55 +0300

    add open drawer button

--- src/Settings.js
+++ src/Settings.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Button } from 'react-native'
 
-const Settings = () => {
+const Settings = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text>Settings Screen v5</Text>
+      <Button
+        title="Open Drawer"
+        onPress={() => navigation.openDrawer()}
+      />
     </View>
   )
 }
